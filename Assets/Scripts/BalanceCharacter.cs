223284fa90fa8441541bbd44de56728d106d83bb
LlanosNing <llanosning@gmail.com>
2026-02-20 12:33:11 +0100

    clase

--- Assets/Scripts/BalanceCharacter.cs
+++ Assets/Scripts/BalanceCharacter.cs
@@ -7,15 +7,77 @@ public class BalanceCharacter : MonoBehaviour
     public Rigidbody ball;
     public Vector3 offset;
     private Animator anim;
+    //todas las partes del ragdoll
+    public Rigidbody[] bodyParts;
+    public float growDuration = 10;
+
+    private float growTimer;
 
     private void Start()
     {
         TryGetComponent(out anim);
+        //buscar todas las parrtes del cuerpo del ragdoll
+        bodyParts = GetComponentsInChildren<Rigidbody>();
+        //de inicio el ragdoll tiene que estar desactivado
+        EnableRagdoll(false);
     }
 
     void Update()
     {
         transform.position = ball.transform.position + offset;
         anim.SetFloat("Direction", Mathf.Sign(ball.velocity.z));
+
+        //ha terminado el temporizador del tamño y estaba agrandado
+        if(Time.time >= growTimer && transform.localScale.x > 1)
+        {
+            //reiniciar el tamaño al valor original
+            transform.localScale = Vector3.one;
+        }
     }
+
+    public void Die()
+    {
+        //activar el ragdoll
+        EnableRagdoll(true);
+        //se desactiva este script para que no siga más a la pelota
+        this.enabled = false;
+    }
+
+    void EnableRagdoll(bool enable)
+    {
+        //activar el animator cuando se desactiva el ragdoll y viceversa
+        anim.enabled = !enable;
+
+        //activar el kinematic cuando se desactiva el ragdoll y viceversa
+        foreach (Rigidbody part in bodyParts) { 
+        part.isKinematic = !enable;
+        }
+    }
+
+    public void Grow()
+    {
+        //aumentar el tamaño del personaje
+        transform.localScale *= 1.1f;
+        //reiniciar el temporizador de volver al tamaño normal  
+        growTimer = Time.time + growDuration;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //si detecta que choca contra algo que lo mata, se muere
+        if(collision.collider.CompareTag("Kill"))
+        {
+            Die();
+        }
+
+        //si detecta una seta, SE LA COME JUAJUAJUAs
+        if (collision.collider.CompareTag("Mushroom"))
+        {
+            Grow();
+            //destruir la setita
+            Destroy(collision.gameObject);
+        }
+    }
+
 }
